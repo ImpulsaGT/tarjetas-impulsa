d7ac61ea0fb7cea1965805cffcac61027838426e
ImpulsaGT <jlopez@impulsa.com.gt>
2026-08-25 09:18:43 -0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,62 @@
+// Datos de cada tarjeta digital.
+//
+// Para agregar a alguien nuevo (sin crear un repositorio nuevo):
+// 1. Sube su foto a la carpeta img/ con el nombre "slug.png"
+//    (minusculas, sin espacios ni acentos, ej: img/juanperez.png).
+// 2. Agrega aqui un bloque nuevo dentro de TARJETAS, usando ese mismo
+//    "slug" como llave.
+// 3. Guarda los cambios. El link de esa persona sera:
+//    https://impulsagt.github.io/tarjetas-impulsa/?p=slug
+
+const TARJETAS = {
+  wernergaitan: {
+    nombreCompleto: "Werner Gaitán",
+    nombre: "Werner",
+    apellido: "Gaitán",
+    telefono: "50252123163",
+    telefonoVisible: "+502 5212 3163",
+    correo: "wgaitan@impulsa.com.gt",
+    puesto: "Asesor de Ventas",
+    imagen: "wernergaitan.png"
+  },
+  ericadonis: {
+    nombreCompleto: "Erica Domis",
+    nombre: "Erica",
+    apellido: "Domis",
+    telefono: "50239974468",
+    telefonoVisible: "+502 3997 4468",
+    correo: "edonis@impulsa.com.gt",
+    puesto: "Asesor de Ventas",
+    imagen: "ericadonis.png"
+  },
+  mariateresa: {
+    nombreCompleto: "María Teresa Gochez",
+    nombre: "María Teresa",
+    apellido: "Gochez",
+    telefono: "50239975761",
+    telefonoVisible: "+502 3997 5761",
+    correo: "mgochez@impulsa.com.gt",
+    puesto: "Asesor de Ventas",
+    imagen: "mariateresa.png"
+  },
+  diego: {
+    nombreCompleto: "Diego Marroquin",
+    nombre: "Diego",
+    apellido: "Marroquin",
+    telefono: "50239975836",
+    telefonoVisible: "+502 3997 5836",
+    correo: "dmarroquin@impulsa.com.gt",
+    puesto: "Asesor de Ventas",
+    imagen: "diego.png"
+  },
+  karlaperez: {
+    nombreCompleto: "Karla Pérez",
+    nombre: "Karla",
+    apellido: "Pérez",
+    telefono: "50239971562",
+    telefonoVisible: "+502 3997 1562",
+    correo: "kperez@impulsa.com.gt",
+    puesto: "Asesor de Ventas",
+    imagen: "karlaperez.png"
+  }
+};
